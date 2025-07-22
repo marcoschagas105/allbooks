ff4c043e42cc6675c60009e81bd6b0b24094d5fd
marcoschagas105 <marcoschagas105@hotmail.com>
2025-07-22 10:21:12 -0300

    Altera o arquivo server.js

--- src/server.js
+++ src/server.js
@@ -18,5 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
+  console.log("Seja Bem-Vindo a API")
+  console.log("Acesse a API pelo endereço http://localhost:8000")
 })
